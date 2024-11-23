10d953e82681acb9887700f756be2c913a4636b3
Arminas03 <arminas.raz@gmail.com>
2024-11-23 14:40:31 +0100

    preliminary LSTM

--- lstm.py
+++ lstm.py
@@ -1,29 +1,46 @@
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
-import keras
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from keras.api.callbacks import TensorBoard
+from keras.api.models import Sequential
+from keras.api.layers import LSTM, Dense
 
 
-def load_data(path):
+def load_data(path, split_percentage):
     data = pd.read_csv(path)
+    i_split = int(split_percentage * len(data))
 
-    x = data[['rv_lag_1', 'rv_lag_5']].values
-    y = data['rv_lead_1']
+    x = data.drop(columns=['date', 'rv_lead_1', 'ticker']).values
+    x = x.reshape((x.shape[0], 1, x.shape[1]))
+    y = data['rv_lead_1'].values
 
-    return x.reshape((x.shape[0], 1, x.shape[1])), y
+    return x[:i_split], y[:i_split], x[i_split:], y[i_split:]
 
-def main():
-    x_train, y_train = load_data('stock_data_train.csv')
 
-    model = keras.Sequential()
+def model_setup():
+    return Sequential([
+        LSTM(8, return_sequences=True),
+        LSTM(4, return_sequences=True),
+        LSTM(1),
+        Dense(1)
+    ])
 
-    model.add(keras.layers.LSTM(8, input_shape=(1, 2)))
-    model.add(keras.layers.LSTM(4, input_shape=(1, 2)))
 
-    model.compile(optimizer='sgd', loss='mse')
-    model.fit(x_train, y_train, epochs=10)
+def train_model(x, y, model: Sequential):
+    model.compile(optimizer='adam', loss='mse')
+    model.fit(x, y, epochs=50)
 
     model.summary()
+
+
+def main():
+    x_train, y_train, x_test, y_test = load_data('stock_data_train.csv', 0.8)
+
+    model = model_setup()
+    train_model(x_train, y_train, model)
+    
+    print(mean_squared_error(y_test, model.predict(x_test)))
 
 
 if __name__ == '__main__':
